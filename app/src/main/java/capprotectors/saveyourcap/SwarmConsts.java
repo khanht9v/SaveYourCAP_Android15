58af9a031e040e8446a41be4a7dea72dc311997e
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2015-06-26 22:08:45 +0800

    Added Swarm offline files
    Added Swarm offline files

---
+++ app/src/main/java/capprotectors/saveyourcap/SwarmConsts.java
@@ -0,0 +1,26 @@
+package capprotectors.saveyourcap;
+
+class SwarmConsts {
+
+	public static class App {
+		public static final int APP_ID = 17981;
+		public static final String APP_AUTH = "b3efa3ee656161523093b42ecad22ae5";
+	}
+
+	public static class Leaderboard {
+	}
+
+	public static class Achievement {
+	}
+
+	public static class StoreCategory {
+	}
+
+	public static class StoreItem {
+	}
+
+	public static class StoreItemListing {
+	}
+
+}
+
