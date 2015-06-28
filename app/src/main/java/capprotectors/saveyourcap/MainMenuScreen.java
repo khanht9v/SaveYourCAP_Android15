fec6fa50dd2fbb776b425551b7b0cc72731bf03f
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2015-06-28 15:00:11 +0800

    Implement all other buttons
    Now fully functional

--- app/src/main/java/capprotectors/saveyourcap/MainMenuScreen.java
+++ app/src/main/java/capprotectors/saveyourcap/MainMenuScreen.java
@@ -1,5 +1,6 @@
 package capprotectors.saveyourcap;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 
@@ -16,15 +17,14 @@ import capprotectors.implementation.AndroidGame;
 public class MainMenuScreen extends Screen {
     public MainMenuScreen(Game game) {
         super(game);
-        Swarm.init((AndroidGame) game, 17981, "b3efa3ee656161523093b42ecad22ae5");
     }
 
     @Override
     public void update(float deltaTime) {
-        Graphics g = game.getGraphics();
+        //Graphics g = game.getGraphics();
         List<TouchEvent> touchEvents = game.getInput().getTouchEvents();
 
-        for (TouchEvent event: touchEvents) {
+        for (TouchEvent event : touchEvents) {
             if (event.type == TouchEvent.TOUCH_UP) {
 
                 if (inBounds(event, 50, 300, 550, 300)) {
@@ -33,17 +33,32 @@ public class MainMenuScreen extends Screen {
                     game.setScreen(new GameScreen(game));
                 }
 
-                if (inBounds(event, 50, 620, 550, 750-620)) {
+                if (inBounds(event, 50, 620, 550, 750 - 620)) {
                     Assets.click.play(1f);
-                    Swarm.showLogin();
+                    Swarm.showDashboard();
                 }
 
-                if (inBounds(event,620, 460, 1230-620, 530-460)) {
+                if (inBounds(event, 620, 295, 1235 - 620, 365 - 295)) {
+                    Assets.click.play(1f);
+                    toggleMusic();
+                }
+
+                if (inBounds(event, 620, 380, 1235 - 620, 440 - 380)) {
+                    Assets.click.play(1f);
+                    Swarm.showAchievements();
+                }
+
+                if (inBounds(event, 620, 460, 1230 - 620, 530 - 460)) {
                     Assets.click.play(1f);
                     Swarm.showLeaderboards();
                 }
 
-                if (inBounds(event, 650, 640, 1230-620, 110 )) {
+                if (inBounds(event, 620, 555, 1235 - 620, 620 - 555)) {
+                    Assets.click.play(1f);
+                    Swarm.showStore();
+                }
+
+                if (inBounds(event, 650, 640, 1230 - 620, 110)) {
                     confirmExit();
                 }
 
@@ -83,25 +98,58 @@ public class MainMenuScreen extends Screen {
         confirmExit();
     }
 
-    public void confirmExit() {
-        AlertDialog.Builder builder = new AlertDialog.Builder((AndroidGame) this.game);
-        builder.setCancelable(false);
-        builder.setMessage("Do you want to Exit?");
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+    public void toggleMusic() {
+        ((AndroidGame) game).runOnUiThread(new Runnable() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                //if user pressed "yes", then he is allowed to exit from application
-                ((AndroidGame) MainMenuScreen.this.game).finish();
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder((AndroidGame) game);
+                builder.setCancelable(true);
+                builder.setMessage("The only setting: Toggle Music :D");
+                builder.setPositiveButton("Music on!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Assets.theme.setVolume(0.85f);
+                        dialog.cancel();
+                    }
+                });
+                builder.setNegativeButton("No music plz", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Assets.theme.setVolume(0);
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
             }
         });
-        builder.setNegativeButton("No",new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                //if user select "No", just cancel this dialog and continue with app
-                dialog.cancel();
-            }
-        });
-        AlertDialog alert=builder.create();
-        alert.show();
     }
+
+    public void confirmExit() {
+        ((AndroidGame) game).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder((AndroidGame) game);
+                builder.setCancelable(false);
+                builder.setMessage("Do you want to Exit?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //if user pressed "yes", then he is allowed to exit from application
+                        ((AndroidGame) MainMenuScreen.this.game).finish();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //if user select "No", just cancel this dialog and continue with app
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+    }
+
 }
